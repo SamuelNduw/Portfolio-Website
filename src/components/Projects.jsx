238df49cd6b60e58b3c4ef23bb1661fa87a5e7f9
SamuelNduw <samuelwamankalnduw@gmail.com>
2025-08-13 18:01:31 +0200

    Added more icons to projects section and changed order of projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ import AirlineRes from '../assets/airlineRes.jpg';
 import BloggingAI from '../assets/Blogging2.png';
 import HockeyApp from '../assets/HockeyAppProjectThumbnail-min.png'
 import { FaReact, FaCss3Alt, FaHtml5, FaJava, FaJs} from 'react-icons/fa';
-import { SiTailwindcss, SiMysql, SiPython, SiFirebase, SiFlutter } from "react-icons/si";
+import { SiTailwindcss, SiMysql, SiPython, SiFirebase, SiFlutter, SiSqlite } from "react-icons/si";
 import { Element } from 'react-scroll';
 import { Fade } from 'react-awesome-reveal';
 
@@ -37,11 +37,13 @@ const Projects = () => {
                     <SiTailwindcss size={30} fill='#38BDF8' />
                     <SiPython size={30} fill='#FFD43B'/>
                     <SiFirebase size={30} fill='#F6820D'/>
+                    <SiSqlite size={30} fill='#FFFFFF'/>
                 </ProjectItem>
                 {/* second card */}
                 <ProjectItem img={HockeyApp} title="Hockey App" codeURL="https://github.com/SamuelNduw/MAP-Project" postLink="https://www.linkedin.com/posts/samuel-nduw-53153126a_mobile-app-development-does-seem-interesting-activity-7359536881400713216-s4Vw?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEHuGLUBAjPdFfRcNXWdJ2wYAVh7TTmYi3k">
                     <SiFlutter size={30} fill='#027DFD' className='shadow-gray-800'/>
                     <SiPython size={30} fill='#FFD43B'/>
+                    <SiMysql size={30} fill='#FFFFFF'/>
                 </ProjectItem>
                 {/* third card */}
                 <ProjectItem img={ChurchSite} title="CLMI Sauyemwa" codeURL="" pageLink="https://christsloveministriessauyemwa.com/">
@@ -49,24 +51,27 @@ const Projects = () => {
                     <SiTailwindcss size={30} fill='#38BDF8' />
                 </ProjectItem>
                 {/* fourth card */}
+                <ProjectItem img={SchoolSite} title="Friendly Private School" pageLink="https://friendlyprivateschool.org/">
+                    <FaReact size={30} fill='#38BDF8' className='shadow-gray-800'/>
+                    <SiTailwindcss size={30} fill='#38BDF8' />
+                    <SiFirebase size={30} fill='#F6820D'/>
+                </ProjectItem>
+                {/* fifth card */}
                 <ProjectItem img={AirlineRes} title="Airline Seat Reservation" codeURL="https://github.com/SamuelNduw/AirlineSeatReservationSystem" pageLink="https://youtu.be/yVX4BLEmU24">
                     <FaJava size={30} fill='red' className='shadow-gray-800'/>
                     <SiMysql size={30} fill='white' className='shadow-gray-800'/>
                 </ProjectItem>
-                {/* fifth card */}
+                {/* sixth card */}
                 <ProjectItem  img={GamingSite} title="Gaming Store" codeURL="https://github.com/SamuelNduw/my-first-project" pageLink="https://samuel-my-first-website.netlify.app">
                     <FaJs size={30} fill='#f0db4f' className='shadow-gray-800'/>
                     <FaCss3Alt size={30} fill='#264de4' />
                     <FaHtml5 size={30} fill='#f06529' />
                 </ProjectItem>
+                {/* seventh card */}
                 <ProjectItem  img={SafariSite} title="Safari Agency" codeURL="https://github.com/SamuelNduw/Safari-Agency-1" pageLink="https://sammy-safari-agency.netlify.app/">
                     <FaJs size={30} fill='#f0db4f' className='shadow-gray-800'/>
                     <SiTailwindcss size={30} fill='#38BDF8' />
                     <FaHtml5 size={30} fill='#f06529' />                    
-                </ProjectItem>
-                <ProjectItem img={SchoolSite} title="Friendly Private School" codeURL="https://github.com/Friendly-Private-School/fps-web" pageLink="https://friendlyprivateschool.org/">
-                    <FaReact size={30} fill='#38BDF8' className='shadow-gray-800'/>
-                    <SiTailwindcss size={30} fill='#38BDF8' />
                 </ProjectItem>
 
             </div>
